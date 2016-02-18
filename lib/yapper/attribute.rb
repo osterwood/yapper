6001983a4b892fcc0e4a415712dea99c6faf2a97
Chris Osterwood <chris@osterwood.com>
2016-02-18 13:24:09 -0500

    Embedding remove_possible_method for Module

--- lib/yapper/attribute.rb
+++ lib/yapper/attribute.rb
@@ -44,6 +44,19 @@ class Array
   end
 end
 
+class Module
+  def remove_possible_method(method)
+    if method_defined?(method) || private_method_defined?(method)
+      undef_method(method)
+    end
+  end
+
+  def redefine_method(method, &block)
+    remove_possible_method(method)
+    define_method(method, &block)
+  end
+end
+
 
 class Class
   # Declare a class-level attribute whose value is inheritable by subclasses.
